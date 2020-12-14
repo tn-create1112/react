d855166322425acb28121add0fc45058527f2544
tn-create1112 <74314427+tn-create1112@users.noreply.github.com>
2020-12-14 18:16:26 +0900

    Update Footer.js

--- Footer.js
+++ Footer.js
@@ -6,7 +6,6 @@ class Footer extends React.Component {
       <div className="footer">
         <div className="footer-inner">
           <div className="footer-logo">
-            <img src="http://flat-icon-design.com/f/f_object_27/s512_f_object_27_0bg.png"/>
             <p>さあ、あなたも投稿してみましょう！</p>
           </div>
           <ul className="footer-list">
